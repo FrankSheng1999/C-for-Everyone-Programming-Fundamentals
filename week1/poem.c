fc6ee644ce9f453db24c26cecf2122bd9b10518b
FrankSheng1999 <65141678+FrankSheng1999@users.noreply.github.com>
2024-04-20 09:02:02 +0800

    Add files via upload

---
+++ week1/poem.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Roses are red \n");
+    printf("Violets are blue \n");
+    printf("Sugar is sweet \n");
+    printf("And so are you");
+
+    return 0;
+}
---
+++ week1/week1 _ correction.c
@@ -0,0 +1,12 @@
+/*
+The '' should be ""
+since '' is only for the single character
+hello world is a string
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    return 0;
+}
